450daac1f0904e2510ec8457a642ab06dc9ffc70
Serafima <serafima93.93@gmail.com>
2022-07-08 17:17:33 +0300

    'second'

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,26 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main/main';
+
+type AppScreenProps = {
+  filmsCount: number[];
+  filmName: string;
+  filmGenre: string;
+  filmDate: number;
+};
+
+function App({
+  filmsCount,
+  filmName,
+  filmGenre,
+  filmDate,
+}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen
+      filmsCount={filmsCount}
+      filmName={filmName}
+      filmGenre={filmGenre}
+      filmDate={filmDate}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Films = {
+  FILMS_COUNT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+};
+
+const FilmDescription = {
+  filmName: 'The Grand Budapest Hotel',
+  filmGenre: 'Drama',
+  filmDate: 2014,
+};
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App
+      filmsCount={Films.FILMS_COUNT}
+      filmName={FilmDescription.filmName}
+      filmGenre={FilmDescription.filmGenre}
+      filmDate={FilmDescription.filmDate}
+    />
+  </React.StrictMode>
 );
